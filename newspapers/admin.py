73b749a27b29f940a07648d1ad8b139c4d19e359
jesus <jesusenrique13721@gmail.com>
2026-05-23 16:01:56 -0400

    model app is done

---
+++ newspapers/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Blog
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title', 'content', 'author__username')
+    list_filter = ('created_at', 'author__username')
+
+
+
+
+
+admin.site.register(Blog, BlogAdmin)
+
