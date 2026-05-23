64fcf0631ad5e32fb0417717af5de8f95dc0afac
jesus <jesusenrique13721@gmail.com>
2026-05-23 16:34:49 -0400

    BlogList is done

--- baseproject/settings.py
+++ baseproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'baseproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
