5508e14a2a29fb73e0d09db6dadd9f3989601e7b
jesus <jesusenrique13721@gmail.com>
2026-05-23 17:22:24 -0400

    blog detail is done

--- newspapers/views.py
+++ newspapers/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from .models import Blog
 
 # Create your views here.
 class BlogListView(ListView):
     model = Blog
     template_name = 'post_list.html'
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'post_detail.html'
