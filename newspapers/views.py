0dec1068003fb4d767e108b7d7158f58641fe762
jesus <jesusenrique13721@gmail.com>
2026-06-06 17:33:28 -0400

    permisos is done

--- newspapers/views.py
+++ newspapers/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from .models import blog
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PostListView(ListView):
     model = blog
     template_name = 'post_list.html'
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = blog
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = blog
     template_name = 'post-create.html'
     fields = ['title', 'content', 'author']
